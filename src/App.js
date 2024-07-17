941ab0cfc1f72c457a242556cbff58a567d8b68c
G-Khalayi <gloria.maloba@strathmore.edu>
2024-07-17 15:07:58 +0300

    prep react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Prep from './components/prep';
+
+function App() {
+  return (
+    <>
+      <Prep />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/prep.css';
+
+function Footer() {
+  return (
+    <section className="footer">
+      <p>
+        254 Madaraka Avenue, Nairobi, Kenya | Phone: 0722 12345 | Email: info@hopebridge.org
+      </p>
+      <p>Address: P.O Box 12345-00100 Nairobi, Kenya</p>
+    </section>
+  );
+}
+
+export default Footer;
